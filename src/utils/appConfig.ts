a8d0e5492762e56868120093eba93191b4805eae
Almo <ali12.mo4@gmail.com>
2026-05-20 12:53:01 +0300

    Update app config and info

---
+++ src/utils/appConfig.ts
@@ -0,0 +1,8 @@
+export const APP_INFO = {
+  name: 'ChatLux',
+  version: '1.0.0',
+  description: 'مساعدك الذكي الشخصي بالذكاء الاصطناعي',
+  developer: 'Ali Mohammed',
+  email: 'ali12.mo4@gmail.com',
+  privacyPolicy: 'https://chatlux-eb685.web.app/privacy',
+};
